a790998fd73993d64de4991b11405753dd65e785
FrancoCRJ <coraje180@gmail.com>
2018-10-09 18:04:53 -0500

    voltaje practica

---
+++ VoltajeADC/AvrBuild.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+del "c:\users\coraje180\desktop\voltajeadc\labels.tmp"
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "c:\users\coraje180\desktop\voltajeadc\labels.tmp" -fI  -o "c:\users\coraje180\desktop\voltajeadc\pvoltaje.hex" -d "c:\users\coraje180\desktop\voltajeadc\pvoltaje.obj" -e "c:\users\coraje180\desktop\voltajeadc\pvoltaje.eep" -m "c:\users\coraje180\desktop\voltajeadc\pvoltaje.map" -W+ie   "C:\Users\Coraje180\Desktop\VoltajeADC\Pvoltaje.asm"
